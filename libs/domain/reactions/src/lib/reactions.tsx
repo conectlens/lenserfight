117c07af0572b79b6009d2a4ce08d0d43bd0a311
Ömer Faruk Coşkun <maintainers@lenserfight.com>
2026-03-13 15:53:45 +0300

    feat: Migrate project structure to Nx monorepo, introducing new features like analytics, admin, leaderboard, and generations, while refactoring existing components and utilities.

---
+++ libs/domain/reactions/src/lib/reactions.tsx
@@ -0,0 +1,11 @@
+import styles from './reactions.module.css'
+
+export function Reactions() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Reactions!</h1>
+    </div>
+  )
+}
+
+export default Reactions
---
+++ libs/features/leaderboard/src/lib/leaderboard.tsx
@@ -0,0 +1,11 @@
+import styles from './leaderboard.module.css'
+
+export function Leaderboard() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Leaderboard!</h1>
+    </div>
+  )
+}
+
+export default Leaderboard
