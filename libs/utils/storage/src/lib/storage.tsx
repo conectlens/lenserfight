184658988cc2328915f086759aa119f2266ba5ba
ÖMER FARUK COŞKUN <ofcskn.omer@gmail.com>
2026-03-13 15:53:45 +0300

    feat: Migrate project structure to Nx monorepo, introducing new features like analytics, admin, leaderboard, and generations, while refactoring existing components and utilities.

---
+++ libs/utils/storage/src/lib/storage.tsx
@@ -0,0 +1,11 @@
+import styles from './storage.module.css'
+
+export function Storage() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Storage!</h1>
+    </div>
+  )
+}
+
+export default Storage
