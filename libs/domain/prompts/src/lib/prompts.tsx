43911fd939aba6ea26f3355bd8967e8e40e50bc3
ÖMER FARUK COŞKUN <ofcskn.omer@gmail.com>
2026-03-13 15:53:45 +0300

    feat: Migrate project structure to Nx monorepo, introducing new features like analytics, admin, leaderboard, and generations, while refactoring existing components and utilities.

---
+++ libs/domain/prompts/src/lib/prompts.tsx
@@ -0,0 +1,11 @@
+import styles from './prompts.module.css'
+
+export function Prompts() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Prompts!</h1>
+    </div>
+  )
+}
+
+export default Prompts
---
+++ libs/infra/analytics/src/lib/analytics.tsx
@@ -0,0 +1,11 @@
+import styles from './analytics.module.css'
+
+export function Analytics() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Analytics!</h1>
+    </div>
+  )
+}
+
+export default Analytics
