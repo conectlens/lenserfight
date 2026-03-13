14db55cddf682df6e0093f777c49969e756fa5e7
ÖMER FARUK COŞKUN <ofcskn.omer@gmail.com>
2026-03-13 15:53:45 +0300

    feat: Migrate project structure to Nx monorepo, introducing new features like analytics, admin, leaderboard, and generations, while refactoring existing components and utilities.

---
+++ libs/api/dto/src/lib/dto.tsx
@@ -0,0 +1,11 @@
+import styles from './dto.module.css'
+
+export function Dto() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Dto!</h1>
+    </div>
+  )
+}
+
+export default Dto
---
+++ libs/domain/user/src/lib/user.tsx
@@ -0,0 +1,11 @@
+import styles from './user.module.css'
+
+export function User() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to User!</h1>
+    </div>
+  )
+}
+
+export default User
