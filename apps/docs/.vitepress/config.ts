0a798d6a55d7943407f49f618b05e1c64f982288
ÖMER FARUK COŞKUN <ofcskn.omer@gmail.com>
2026-03-14 14:33:47 +0300

    feat: Add a comprehensive set of favicons and update `index.html` to reference them, replacing the old favicon.

---
+++ apps/docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  srcDir: "../../docs",
+  
+  title: "LenserFight Documentation - AI Driven Challenges",
+  description: "A Community Tool"
+})
