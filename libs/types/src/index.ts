d5c72fd9c76343b04857d134731eb6a93dca16cb
ÖMER FARUK COŞKUN <ofcskn.omer@gmail.com>
2026-03-22 07:04:12 +0300

    feat: Export `ReportReasonEnum` and `REPORT_REASONS` for content reporting.

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -46,6 +46,8 @@ export type {
 export type { LenserBadge as ProfileLenserBadge } from './lib/lenser.types'
 export type {
   ContentReportDTO,
+  ReportReasonEnum,
+  REPORT_REASONS,
   ContentType,
   SortOption,
   TagActivityEventDTO,
