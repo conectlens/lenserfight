29f58fed93cffe3f4348e2d69a1f4e9d1bcbe8ff
Ömer Faruk Coşkun <maintainers@lenserfight.com>
2026-05-05 15:35:27 +0300

    refactor: replace direct lenser hooks with optional hook and remove redundant currentUserId prop

--- libs/features/battles/src/lib/pages/BattleDetailPage.tsx
+++ libs/features/battles/src/lib/pages/BattleDetailPage.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { useLenser } from '@lenserfight/features/profile'
 import { ImmersiveArenaView } from '../components/arena/ImmersiveArenaView'
 
 export function BattleDetailPage() {
   const { slug } = useParams<{ slug: string }>()
-  const { lenser } = useLenser()
 
-  return (
-    <ImmersiveArenaView
-      slug={slug ?? ''}
-      currentUserId={lenser?.id}
-    />
-  )
+  return <ImmersiveArenaView slug={slug ?? ''} />
 }
