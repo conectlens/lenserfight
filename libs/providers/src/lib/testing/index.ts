3d94946a8efde817a01e1c1f3e0bfd88ce59a7c1
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-10 04:16:58 +0300

    feat: add MediaGallery component and export testing utilities for media providers

---
+++ libs/providers/src/lib/testing/index.ts
@@ -0,0 +1,9 @@
+export {
+  makePendingResult,
+  makeCompletedImageResult,
+  makeCompletedVideoResult,
+  makeCompletedAudioResult,
+  mockFetch,
+  resetFetchMock,
+} from './media-provider-factory'
+export type { FetchMockRoute } from './media-provider-factory'
