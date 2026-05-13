0f70a565aad0f9e1b0a3e1f0c2329622a77d5478
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-13 09:44:38 +0300

    refactor: update libs/types/src/lib/partner-provisioning.types.ts

--- libs/types/src/lib/partner-provisioning.types.ts
+++ libs/types/src/lib/partner-provisioning.types.ts
@@ -1,3 +1,20 @@
+export type ChainabitConnectionState =
+  | 'loading'
+  | 'no_account'
+  | 'invalid_connection'
+  | 'no_credits'
+  | 'connected'
+
+export interface ChainabitAiModel {
+  id: string
+  modelKey: string
+  name: string
+  provider: string
+  capabilities: string[]
+  active: boolean
+  costPer1kTokens?: number
+}
+
 export interface PartnerProvisionRecord {
   partnerName: string
   displayName: string
