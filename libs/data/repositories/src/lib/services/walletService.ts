e32e9f2fe752eb25feba94aea2a468a192bcede7
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-13 09:44:37 +0300

    refactor: update libs/data/repositories/src/lib/services/walletService.ts

--- libs/data/repositories/src/lib/services/walletService.ts
+++ libs/data/repositories/src/lib/services/walletService.ts
@@ -2,17 +2,14 @@ import {
   ExecuteByokRequest,
   ExecuteImageRequest,
   ExecuteRequest,
-  WalletCheckoutRequest,
 } from '@lenserfight/types'
 import { walletApiClient } from '../repositories/walletApiClient'
 
 export const walletService = {
   getBalance: () => walletApiClient.getBalance(),
-  getProducts: () => walletApiClient.getProducts().then((r) => r.products),
   getTransactions: (page?: number, limit?: number) =>
     walletApiClient.getTransactions(page, limit),
   getPricing: () => walletApiClient.getPricing().then((r) => r.models),
-  checkout: (req: WalletCheckoutRequest) => walletApiClient.checkout(req),
   execute: (req: ExecuteRequest) => walletApiClient.execute(req),
   executeByok: (req: ExecuteByokRequest) => walletApiClient.executeByok(req),
   executeImage: (req: ExecuteImageRequest) => walletApiClient.executeImage(req),
