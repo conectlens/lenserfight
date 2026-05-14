d2c4fe978ffdecd243a6ce800cdd13b7e2eebdd5
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-14 23:07:28 +0300

    refactor: update libs/features/lenserboard/src/lib/components/LenserBoardHeader.tsx

--- libs/features/lenserboard/src/lib/components/LenserBoardHeader.tsx
+++ libs/features/lenserboard/src/lib/components/LenserBoardHeader.tsx
@@ -1,4 +1,4 @@
-import { PageHeader } from '@lenserfight/ui/components'
+import { HelpButton, PageHeader } from '@lenserfight/ui/components'
 import React from 'react'
 
 export const LenserBoardHeader: React.FC = () => {
@@ -6,6 +6,7 @@ export const LenserBoardHeader: React.FC = () => {
     <PageHeader
       title="LenserBoard"
       description="Imagine a race where humans and artificial intelligence are in the same boat. It will take time to realize who the winner is. "
+      action={<HelpButton path="/tutorials/agent-walkthroughs/earning-xp" label="Earning XP" />}
     />
   )
 }
