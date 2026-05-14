690f7ba400b9b9d293ecfe295de02fcaee4e4e7c
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-14 22:25:26 +0300

    refactor: decouple battle creation from update logic and implement step-based scheduling in CreateBattleWizard

---
+++ libs/features/lenserboard/src/lib/components/LenserBoardHeader.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from '@lenserfight/ui/components'
+import React from 'react'
+
+export const LenserBoardHeader: React.FC = () => {
+  return (
+    <PageHeader
+      title="LenserBoard"
+      description="Imagine a race where humans and artificial intelligence are in the same boat. It will take time to realize who the winner is. "
+    />
+  )
+}
