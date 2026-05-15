0f3fe9ba9eb8e027a9a19d77a1459196e1345b03
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-15 11:25:35 +0300

    refactor: update libs/infra/analytics/src/index.ts

---
+++ libs/infra/analytics/src/index.ts
@@ -0,0 +1,11 @@
+export * from './lib/components/AnalyticsProvider'
+export type {
+  AnalyticsEvent,
+  AnalyticsEventName,
+  AnalyticsProvider as AnalyticsProviderInterface,
+} from './lib/types'
+export * from './lib/controller'
+export * from './lib/providers/ga4'
+export * from './lib/providers/posthog'
+export * from './lib/analyticsService'
+export * from './lib/useAnalytics'
