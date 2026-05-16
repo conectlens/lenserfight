f2bbe8f9914f2998fde508f8720840540bc73177
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-17 01:03:47 +0300

    feat: add libs/infra/execution/src/lib/catalog/

---
+++ libs/infra/execution/src/lib/catalog/index.ts
@@ -0,0 +1,36 @@
+export {
+  WORKFLOW_NODE_CATALOG,
+  WORKFLOW_NODE_CATEGORIES,
+  areWorkflowNodesCompatible,
+  buildWorkflowNodeCardMetadata,
+  getWorkflowNodeCatalogEntry,
+  getWorkflowNodeCategoryColor,
+  getWorkflowNodeCategoryCounts,
+  getWorkflowNodeCategoryIcon,
+  getWorkflowNodeCategoryLabel,
+  getWorkflowNodeCompatibilityWarning,
+  getWorkflowNodesByCategory,
+  isWorkflowUtilityNodeType,
+  searchWorkflowNodeCatalog,
+  validateWorkflowNodeCatalog,
+} from './workflow-node-catalog'
+export type {
+  WorkflowCatalogNodeType,
+  WorkflowExecutionEnvironment,
+  WorkflowFundingMode,
+  WorkflowNodeCardMetadata,
+  WorkflowNodeCatalogEntry,
+  WorkflowNodeCategory,
+  WorkflowNodeConfigExample,
+  WorkflowNodeConfigField,
+  WorkflowNodeConfigKind,
+  WorkflowNodeErrorBehavior,
+  WorkflowNodeIOType,
+  WorkflowNodeN8nMapping,
+  WorkflowNodeRetryBehavior,
+  WorkflowNodeSchemaField,
+} from './workflow-node-catalog'
+export {
+  normalizeWorkflowNodeConfigForExecution,
+} from './workflow-node-config-normalizer'
+export type { ExecutableWorkflowNodeConfig } from './workflow-node-config-normalizer'
