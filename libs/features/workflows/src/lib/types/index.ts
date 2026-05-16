d9dbac5f59c30670cea53bee6913edd9b1c03a23
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-17 01:03:47 +0300

    feat: add libs/features/workflows/src/lib/types/

---
+++ libs/features/workflows/src/lib/types/index.ts
@@ -0,0 +1,10 @@
+export type {
+  WorkflowNodeConfig,
+  WorkflowNodeData,
+  RunnerConfigFieldType,
+  RunnerConfigFieldDescriptor,
+  RunnerOutputField,
+  RunnerConfigCategory,
+  RunnerConfigDescriptor,
+  RunnerConfigFormProps,
+} from './workflow-node.types'
