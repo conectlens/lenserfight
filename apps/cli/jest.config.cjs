f74e0ab78b96bff90eb62783fa9022ccf380fd1b
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-28 02:01:01 +0300

    feat: expand SDK with new client modules for agents, lenses, and protocols, including supporting types and database migrations.

--- apps/cli/jest.config.cjs
+++ apps/cli/jest.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(citty|@lenserfight/domain)/)',
+  ],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/cli',
   moduleNameMapper: {
