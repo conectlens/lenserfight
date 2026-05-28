e4d6aade43bcc1e3a8a6ad5bc65c008ebcbe925b
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-28 22:44:59 +0300

    feat: add libs/types/src/lib/contact.types.ts

---
+++ libs/types/src/lib/contact.types.ts
@@ -0,0 +1,22 @@
+export interface ContactMessage {
+  id: string
+  lenser_id?: string | null
+  name: string
+  email: string
+  subject: string
+  message: string
+  kvkk_approved: boolean
+  user_agent?: string | null
+  ip_address?: string | null
+  created_at: string
+}
+
+export interface CreateContactDTO {
+  name: string
+  email: string
+  subject: string
+  message: string
+  kvkk_approved: boolean
+  user_agent?: string
+  ip_address?: string
+}
