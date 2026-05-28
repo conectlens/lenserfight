7f9baa54a60c4d416909b6e22f677816a50fb7d0
Ömer Faruk Coşkun <ofcskn.omer@gmail.com>
2026-05-28 22:45:08 +0300

    feat: add libs/utils/number/

---
+++ libs/utils/number/src/lib/numberUtils.ts
@@ -0,0 +1,9 @@
+export const formatCount = (num: number): string => {
+  if (num === 0) return '0'
+  if (num < 1000) return num.toString()
+  if (num < 1000000) {
+    // 1500 -> 1.5K
+    return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K'
+  }
+  return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M'
+}
